a66b669b70139df76616d0d41bae7408b6712ae0
yu <yuc55577@gmail.com>
2024-01-04 16:10:59 +0800

    [新增] 打开文件失败

---
+++ FileIO/FileIO.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <error.h>
+
+
+int main()
+{
+
+    /* 打开文件 */
+    int fd = open("./hello.txt", O_RDWR, 0644);
+    if(fd == -1)
+    {
+        perror("open error\n");
+    }
+
+
+    return 0;
+}
