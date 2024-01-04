824bd4a3b6401352082fa5854848a6aa92d6440f
yu <yuc55577@gmail.com>
2024-01-04 16:24:11 +0800

    [新增] 验证文件描述符占用的是最小的一个空闲描述符

--- FileIO/FileIO.c
+++ FileIO/FileIO.c
@@ -3,18 +3,58 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <error.h>
-
+#include <unistd.h>
 
 int main()
 {
 
     /* 打开文件 */
-    int fd = open("./hello.txt", O_RDWR, 0644);
-    if(fd == -1)
+    int fd1 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if(fd1 == -1)
     {
         perror("open error\n");
     }
+    printf("fd1 = %d\n", fd1);
 
+
+    /* 打开文件 */
+    int fd2 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if(fd2 == -1)
+    {
+        perror("open error\n");
+    }
+    printf("fd2 = %d\n", fd2);
+
+    /* 打开文件 */
+    int fd3 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if(fd3 == -1)
+    {
+        perror("open error\n");
+    }
+    printf("fd3 = %d\n", fd3);
+
+
+    /* 打开文件 */
+    int fd4 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if(fd4 == -1)
+    {
+        perror("open error\n");
+    }
+    printf("fd4 = %d\n", fd4);
+
+
+    /* 关闭文件fd1 */
+    close(fd1);
+
+    /* 打开文件 */
+    int fd5 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if(fd5 == -1)
+    {
+        perror("open error\n");
+    }
+    printf("fd5 = %d\n", fd5);
+    
+    /* 关闭文件描述符 */
 
     return 0;
 }
