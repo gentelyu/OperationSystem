946f4375b400f0e7d46dcf2a359be0b9d0e5a840
yu <yuc55577@gmail.com>
2024-01-15 15:03:10 +0800

    [新增] 完成对线程池的初始化

--- threadPool/threadPool.h
+++ threadPool/threadPool.h
@@ -1,21 +1,25 @@
-#ifndef __THREAD_H_
-#define __THREAD_H_
+#ifndef __THREAD_POOL_H_
+#define __THREAD_POOL_H_
 
 #include <pthread.h>
 
+/* 线程池结构体 */
 typedef struct threadpool_t
 {
     /* 线程池中的线程 */
-    pthread_t threadId;
+    pthread_t *threadIds;
 
     /* 最小的线程数 */
     int minThreads;
-
-    /* 最大线程数 */
+    /* 最大的线程数 */
     int maxThreads;
-
-
 }threadpool_t;
 
+/* 线程池初始化 */
+int threadPoolInit(threadpool_t *pool, int minThreads, int maxThreads);
 
-#endif
+/* 线程池销毁 */
+int threadPoolDestroy(threadpool_t *pool);
+
+
+#endif // __THREAD_POOL_H_
