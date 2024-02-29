66adee77662d2eb24e7a50415955c853bf696801
yu <yuc55577@gmail.com>
2024-02-29 10:25:46 +0800

    [新增]父子进程的执行顺序、退出、等待情况

--- 进程/demoExit.c
+++ 进程/demoExit.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h>         // _exit()
 #include <errno.h>
-#include <stdlib.h>
+#include <stdlib.h>         // exit()
 #include <sys/types.h>
+#include <sys/wait.h>       // wait()
+
+/* 
+    _exit()是系统调用，它会直接退出程序并返回一个整数值给操作系统，
+    exit()则是一个库函数，在退出程序之前还会执行一些清理工作，如关闭文件、释放内存等，底层封装的是_exit()。
+    exit()可以使用atexit()注册的函数，更安全*/
 
 int main()
 {
+    int count = 0;
+
     /* 创建进程 */
     pid_t pid = fork();
     if (pid < 0)
@@ -18,7 +26,13 @@ int main()
     {
         /* 子进程 */
         pid_t childPid = getpid();
-        printf("childPid:%d\n", childPid);
+        
+        while(count < 10)
+        {
+            count++;
+            printf("childPid:%d\n", childPid);
+            sleep(2);
+        }
 
         /* 子进程退出 */
         _exit(0);
@@ -27,7 +41,16 @@ int main()
     {
         /* 父进程 */
         pid_t parentPid = getpid();
-        printf("parentPid:%d\n", parentPid);
+
+        while(count < 5)
+        {
+            count++;
+            printf("parentPid:%d\n", parentPid);
+            sleep(2);
+        }
+        
+        /* 等待子进程退出 */
+        wait(NULL);
     }
     /* 子进程已经结束了, 那么子进程的代码就不会到这边了. */
     
